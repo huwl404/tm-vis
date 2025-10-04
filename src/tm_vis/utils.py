c2f868221a375b31839549805c219d3981162a88
hemomona <18146856052@163.com>
2025-10-04 10:34:02 +0800

    fix bug of pattern with *

--- src/tm_vis/utils.py
+++ src/tm_vis/utils.py
@@ -102,8 +102,8 @@ def get_absolute_particle_positions(tomogram_file: str, particle_files: list[Pat
 
 
 def find_particles_file(tomogram_file: Path, particle_files: list[Path], tomogram_matching_pattern) -> Path | None:
-    # 去掉 tomogram 的特殊后缀（如 "_10.000Apx.mrc"）
-    tomo_core = tomogram_file.stem.replace(tomogram_matching_pattern, "")
+    # tmp 后缀为 "*_10.000Apx.mrc"）
+    tomo_core = tomogram_file.stem.replace(tomogram_matching_pattern[1:], "")
     matching_files = [f for f in particle_files if f.name.startswith(tomo_core)]
 
     if len(matching_files) == 1:
