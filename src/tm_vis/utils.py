7e0d6506bfc34359f0aeb5b63ab33afc86c59405
hemomona <18146856052@163.com>
2025-10-04 10:19:17 +0800

    make all parameters optional

--- src/tm_vis/utils.py
+++ src/tm_vis/utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from pathlib import Path
 
 import mrcfile
@@ -7,19 +9,19 @@ import starfile
 
 
 def update_volume_layers(
-    viewer: napari.Viewer,
-    volume: np.ndarray,
-    correlation_volume: np.ndarray,
-    load_volumes=True
+        viewer: napari.Viewer,
+        volume: np.ndarray
 ):
-    if not load_volumes:
-        return
-
     if 'tomogram' not in viewer.layers:
         viewer.add_image(data=volume, name='tomogram', colormap='gray_r')
     else:
         viewer.layers['tomogram'].data = volume
 
+
+def update_corvol_layers(
+        viewer: napari.Viewer,
+        correlation_volume: np.ndarray
+):
     if 'correlation_volume' not in viewer.layers:
         viewer.add_image(
             data=correlation_volume,
@@ -33,10 +35,10 @@ def update_volume_layers(
 
 
 def update_particle_layer(
-    viewer: napari.Viewer,
-    zyx: np.ndarray,
-    cc: np.ndarray,
-    tomogram_name: str
+        viewer: napari.Viewer,
+        zyx: np.ndarray,
+        cc: np.ndarray,
+        tomogram_name: str
 ):
     if 'particles' not in viewer.layers:
         viewer.add_points(
@@ -59,8 +61,33 @@ def update_particle_layer(
         viewer.layers['particles'].metadata['ts_id'] = tomogram_name
 
 
-def get_particle_positions_and_cc(tomogram_file: str, particle_files: list[Path]) -> np.ndarray:
-    df = starfile.read(find_particles_file(Path(tomogram_file), particle_files=particle_files))
+def update_particle_layer_withoutcc(
+        viewer: napari.Viewer,
+        zyx: np.ndarray,
+        bin: float,
+        tomogram_name: str
+):
+    if 'particles' not in viewer.layers:
+        viewer.add_points(
+            zyx / bin,
+            name='particles',
+            size=40 / bin,
+            metadata={
+                'positions': zyx / bin,
+                'ts_id': tomogram_name
+            },
+            face_color='orange',
+            opacity=0.5,
+            out_of_slice_display=True,
+        )
+    else:
+        viewer.layers['particles'].data = zyx / bin
+        viewer.layers['particles'].metadata['positions'] = zyx / bin
+        viewer.layers['particles'].metadata['ts_id'] = tomogram_name
+
+
+def get_particle_positions_and_cc(tomogram_file: str, particle_files: list[Path], tomogram_matching_pattern) -> np.ndarray:
+    df = starfile.read(find_particles_file(Path(tomogram_file), particle_files, tomogram_matching_pattern))
     zyx = df[['rlnCoordinateZ', 'rlnCoordinateY', 'rlnCoordinateX']].to_numpy()
     cc = df['rlnAutopickFigureOfMerit'].to_numpy()
     with mrcfile.open(tomogram_file, header_only=True) as mrc:
@@ -68,11 +95,17 @@ def get_particle_positions_and_cc(tomogram_file: str, particle_files: list[Path]
     return zyx * np.array([nz, ny, nx]), cc
 
 
-def find_particles_file(tomogram_file: Path, particle_files: list[Path]) -> Path | None:
-    matching_files = [
-        f for f in particle_files
-        if f.name.startswith(tomogram_file.stem)
-    ]
+def get_absolute_particle_positions(tomogram_file: str, particle_files: list[Path], tomogram_matching_pattern) -> np.ndarray:
+    df = starfile.read(find_particles_file(Path(tomogram_file), particle_files, tomogram_matching_pattern))
+    zyx = df[['rlnCoordinateZ', 'rlnCoordinateY', 'rlnCoordinateX']].to_numpy()
+    return zyx
+
+
+def find_particles_file(tomogram_file: Path, particle_files: list[Path], tomogram_matching_pattern) -> Path | None:
+    # 去掉 tomogram 的特殊后缀（如 "_10.000Apx.mrc"）
+    tomo_core = tomogram_file.stem.replace(tomogram_matching_pattern, "")
+    matching_files = [f for f in particle_files if if f.name.startswith(tomo_core)]
+
     if len(matching_files) == 1:
         return matching_files[0]
     else:
